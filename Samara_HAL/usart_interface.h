2cd59bf082ceb3cc43ba71a634f73ad03b6e3597
sociotorn <sociotorn@gmail.com>
2024-04-11 14:19:18 -0500

    switch usart to request/release model

--- Samara_HAL/usart_interface.h
+++ Samara_HAL/usart_interface.h
@@ -1,19 +1,19 @@
 #ifndef USART_INTERFACE_H
 #define USART_INTERFACE_H
 
+#include "common_interface.h"
+
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-#include <stdint.h>
-
 typedef enum
 {
-    USART_LENGTH_7,
-    USART_LENGTH_8,
-    USART_LENGTH_9,
-    USART_LENGTH_10
+    USART_LENGTH_7=7,
+    USART_LENGTH_8=8,
+    USART_LENGTH_9=9,
+    USART_LENGTH_10=10
 } __usart_length_t;
 
 typedef enum
@@ -35,30 +35,56 @@ typedef enum
     USART_POLARITY_INVERTED
 } __usart_polarity_t;
 
-typedef struct
+typedef enum
+{
+    USART_DIRECTION_BI,
+    USART_DIRECTION_RX,
+    USART_DIRECTION_TX
+}__usart_direction_t;
+
+typedef struct usart_config
 {
     uint32_t baudRate;
     __usart_length_t length;
     __usart_parity_t parity;
     __usart_stopBits_t stopBits;
     __usart_polarity_t polarity;
-} __usart_config_t;
+} usart_config_t;
+
+typedef struct
+{
+    unsigned int baseAddr;
+    uint8_t periphNum;
+
+    __usart_direction_t dir;
+
+    usart_config_t usartConfig;
+    dma_config_t dmaTXConfig;
+    dma_config_t dmaRXConfig;
+} usart_port_t;
 
 struct __usart_interface
 {
-    void (*init)(void);
-    void (*deinit)(void);
+    bool (*request)(usart_port_t* usartPort, uint8_t periphNum, __usart_direction_t dir);
+    void (*release)(usart_port_t* usartPort);
 
-    uint32_t (*setConfig)(__usart_config_t* config);
+    void (*init)(usart_port_t* usartPort);
+    void (*deinit)(usart_port_t* usartPort);
 
-    uint32_t (*writeBlocking)(uint8_t data);
-    uint32_t (*writeStreamBlocking)(uint8_t* data, uint32_t length);
-    uint32_t (*write)(uint8_t data);
-    uint32_t (*writeStream)(uint8_t* data, uint32_t length);
+    bool (*setConfig)(usart_port_t* usartPort, usart_config_t* usartConfig);
+    bool (*enableTXDMA)(usart_port_t* usartPort, dma_config_t* dmaConfig);
+    bool (*enableRXDMA)(usart_port_t* usartPort, dma_config_t* dmaConfig);
 
-    uint32_t (*dataAvailable)(void);
+    bool (*enable)(usart_port_t* usartPort);
+    bool (*disable)(usart_port_t* usartPort);
+
+    /* Blocking */
+    bool (*write)(uint8_t data);
+    bool (*writeStream)(uint8_t* data, uint32_t length);
+
+    uint32_t (*rxCount)(void);
     uint8_t (*read)(void);
-    uint32_t (*readStream)(uint8_t* data, uint32_t length);
+    bool (*readStream)(uint8_t* data, uint32_t length);
 };
 #ifdef __cplusplus
 }
