1aa4f8c1c6d91181e7a5b06cb44484a4e0366595
sociotorn <sociotorn@gmail.com>
2024-04-11 14:19:07 -0500

    switch gpio to request/release model

--- Samara_HAL/gpio_interface.h
+++ Samara_HAL/gpio_interface.h
@@ -1,7 +1,7 @@
 #ifndef GPIO_INTERFACE_H
 #define GPIO_INTERFACE_H
 
-#include <stdint.h>
+#include "common_interface.h"
 
 #ifdef __cplusplus
 extern "C"
@@ -29,17 +29,36 @@ typedef enum
     GPIO_IRQ_EDGE
 }__gpio_irq_t;
 
+typedef struct gpio_port
+{
+    unsigned int baseAddr;
+    uint8_t periphNum;
+
+    unsigned int padMask;
+
+    __gpio_direction_t  dir:1;
+    __gpio_val_t  val:1;
+
+    __gpio_irq_t irqType;
+
+    unsigned int (*irqCallback)(void);
+
+}gpio_port_t;
+
 struct __gpio_interface
 {
-    void (*init)(void);
-    void (*deinit)(void);
+    bool (*request)(gpio_port_t* gpioPort, uint8_t periphNum, unsigned int padMask);
+    void (*release)(gpio_port_t* gpioPort);
 
-    void (*setDir)(__gpio_direction_t dir);
-    uint32_t (*read)(uint8_t padOffset);
-    uint32_t (*write)(uint8_t padOffset,__gpio_val_t val);
-    uint32_t (*toggle)(uint8_t padOffset);
+    void (*init)(gpio_port_t* gpioPort);
+    void (*deinit)(gpio_port_t* gpioPort);
 
-    void (*attachIrq)(uint8_t padOffset, __gpio_irq_t irqType);
+    void (*setDir)(gpio_port_t* gpioPort, __gpio_direction_t dir);
+    uint8_t (*read)(gpio_port_t* gpioPort);
+    void (*write)(gpio_port_t* gpioPort,__gpio_val_t val);
+    void (*toggle)(gpio_port_t* gpioPort);
+
+    void (*attachIrq)(gpio_port_t* gpioPort, __gpio_irq_t irqType);
     void (*setIRQCallback)(uint32_t (*callback)(void));
 };
 
